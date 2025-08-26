265d0146bf1dc7fb25fd3f395c0d703af76ac704
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-25 21:48:02 -0300

    palavras
    nesse commit inserir as palavras que serão utilizadas no jogo da forca

---
+++ palavras.py
@@ -0,0 +1,19 @@
+palavras = [
+    # Animais
+    "gato", "cachorro", "elefante", "girafa", "tigre", "leão", "coelho", "macaco", "urso", "pinguim",
+
+    # Frutas
+    "banana", "uva", "abacaxi", "laranja", "morango", "melancia", "manga", "kiwi", "pera", "cereja",
+
+    # Objetos
+    "cadeira", "mesa", "computador", "janela", "telefone", "livro", "caneta", "teclado", "foguete", "relógio",
+
+    # Cores
+    "azul", "vermelho", "amarelo", "verde", "roxo", "preto", "branco", "cinza", "laranja", "rosa",
+
+    # Países
+    "brasil", "argentina", "canada", "japao", "franca", "alemanha", "italia", "espanha", "mexico", "portugal",
+
+    # Profissões
+    "professor", "engenheiro", "medico", "advogado", "artista", "programador", "piloto", "bombeiro", "policial", "jornalista"
+]
